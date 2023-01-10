fd664e5599af6db7a08b35c8619ece261fb7b666
marce <colummarcelo@gmail.com>
2023-01-10 02:12:29 -0300

    style toogle

--- src/component/TaskList.jsx
+++ src/component/TaskList.jsx
@@ -86,10 +86,12 @@ function TaskList() {
    dark:peer-focus:ring-cyan-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full
     peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px]
      after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-cyan-600"
-                ></div>
-                <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                  Toggle me
-                </span>
+                ></div>                
+                  {task.completed
+                  ? <span className="ml-3 text-md font-medium text-gray-300">SI</span>
+                  : <span className="ml-3 text-md font-medium text-gray-300">NO</span>
+                  }
+                
               </label>
             </div>
           </div>
