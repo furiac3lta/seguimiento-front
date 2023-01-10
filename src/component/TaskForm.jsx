96b1665877e0b758e0b5c846e1f55724e83185ab
marce <colummarcelo@gmail.com>
2023-01-10 09:14:42 -0300

    changes in taskform

--- src/component/TaskForm.jsx
+++ src/component/TaskForm.jsx
@@ -25,9 +25,9 @@ function TaskForm() {
   <>
     <h1 className='text-cyan-500 text-center text-xl font-extrabold my-2 py-2'>Seguimiento Retiro Materiales</h1>
    <form id="formulario" onSubmit={ handleSubmit } className="bg-zinc-700 max-w-sm p-4 rounded-md"> 
-    <input type="text" name='name' className="w-full p-2 rounded-md bg-zinc-600 mb-2" placeholder='MC'/>
+    <input type="text" name='name' className="w-full p-2 rounded-md bg-zinc-600 mb-2" placeholder='OC'/>
     <input type="text" name='description'className="w-full p-2 rounded-md bg-zinc-600 mb-2" placeholder='Descripcion'/>
-    <button className="bg-indigo-600 px-4 py-1 rounded-sm mx-4">Agregar MC</button>
+    <button className="bg-indigo-600 px-4 py-1 rounded-sm mx-4">Agregar OC</button>
     <label className="px-3">Retirado</label>
     <input type="checkbox" name='completed' className="p-2 rounded-md bg-zinc-600"/> 
    </form>
