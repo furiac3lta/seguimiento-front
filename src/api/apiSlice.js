24a5b5a45854e3a5e34f217b9c052f10b7a74b0e
furiac3lta <colummarcelo@gmail.com>
2024-11-02 18:00:23 -0300

    change slice config

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://ordertacker-4162d2fd5cab.herokuapp.com//api/v1",
+    baseUrl: "http://localhost:8080/seguimiento/api/v1",
   }),
   endpoints: (builder) => ({
 
