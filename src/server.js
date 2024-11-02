784e46607ef9bb26d44668f0b5a9353c723e9b0f
furiac3lta <colummarcelo@gmail.com>
2024-11-02 18:24:11 -0300

    add express

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Sirve los archivos estáticos del build de Vite
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Redirige todas las rutas al index.html para manejar una SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
